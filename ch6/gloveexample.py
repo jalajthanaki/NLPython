6e82fad51539555c562ed63a370d7c347c69f10d
jalajthanaki <jalajthanaki@gmail.com>
2017-06-12 07:11:12 +0530

    updata

--- ch6/gloveexample.py
+++ ch6/gloveexample.py
@@ -2,6 +2,12 @@ import itertools
 from gensim.models.word2vec import Text8Corpus
 from glove import Corpus, Glove
 
+# for installing text8 corpus you should follow this commands
+
+# wget http://mattmahoney.net/dc/text8.zip -P /tmp
+# unzip text8.zip
+
+
 sentences = list(itertools.islice(Text8Corpus('/tmp/text8'), None))
 corpus = Corpus()
 corpus.fit(sentences, window=10)
